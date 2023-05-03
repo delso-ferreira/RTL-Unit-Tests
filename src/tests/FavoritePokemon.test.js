e73cc881501607ce39a625fb5ada04a67b2099f2
delso-ferreira <delsofelipe@gmail.com>
2023-05-03 16:16:36 -0300

    att requisito 03

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { FavoritePokemon /* PokemonDetails */ } from '../pages';
+import pokemonList from '../data';
+
+describe('Testing Favorites', () => {
+  const renderWithRouter = (component) => {
+    const history = createMemoryHistory();
+    return ({
+      ...render(<Router history={ history }>{component}</Router>), history,
+    });
+  };
+
+  it('Test message No Favorite Pokemon Found if theres no favorite pokemon', () => {
+    renderWithRouter(<FavoritePokemon pokemonList={ [] } />);
+
+    const favoriteTitle = screen.getByText(/No favorite Pokémon found/i);
+
+    expect(favoriteTitle).toBeInTheDocument();
+  });
+
+  it('Test if the favorite pokemon appears in the list', () => {
+    renderWithRouter(<FavoritePokemon pokemonList={ [pokemonList[1]] } />);
+
+    screen.getByText(/Charmander/i);
+  });
+});
